afae915dfddb3dc7d3e180c23c1fb97d1a670718
Justin Odel <odel.justin@gmail.com>
2021-06-10 23:04:42 -0400

    created first page including background as well as 3 buttons to select which math game(add, subtract, multiply) the user would like to play

--- app/src/main/java/com/example/mathmanialite/MainActivity.java
+++ app/src/main/java/com/example/mathmanialite/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.mathmanialite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addition;
+    Button subtraction;
+    Button multi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addition = findViewById(R.id.buttonAdd);
+        subtraction = findViewById(R.id.buttonSub);
+        multi = findViewById(R.id.buttonMulti);
+
+        addition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Game.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
