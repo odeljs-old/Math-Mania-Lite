82bf1b7427fcc2a00ed1279e4a7b8bb328ff23bd
Justin Odel <odel.justin@gmail.com>
2021-06-15 20:04:30 -0400

    Addition of Java code to provide timer to game

--- app/src/main/java/com/example/mathmanialite/Game.java
+++ app/src/main/java/com/example/mathmanialite/Game.java
@@ -3,11 +3,13 @@ package com.example.mathmanialite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
 import java.util.Random;
 
 public class Game extends AppCompatActivity {
@@ -30,6 +32,10 @@ public class Game extends AppCompatActivity {
     int userScore = 0;
     int userLife = 3;
 
+    CountDownTimer timer;
+    private static final long START_TIMER_IN_MILIS = 6000;
+    Boolean timer_running;
+    long time_left_in_milis = START_TIMER_IN_MILIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +57,9 @@ public class Game extends AppCompatActivity {
             public void onClick(View v) {
 
                 userAnswer = Integer.valueOf(answer.getText().toString());
+
+                pauseTimer();
+
                 if(userAnswer == realAnswer){
 
                     userScore = userScore + 10;
@@ -75,6 +84,7 @@ public class Game extends AppCompatActivity {
 
                 answer.setText("");
                 gameContinue();
+                resetTimer();
 
             }
         });
@@ -88,6 +98,60 @@ public class Game extends AppCompatActivity {
         realAnswer = number1 + number2;
 
         question.setText(number1 + "+" + number2);
+        startTimer();
+
+    }
+
+    public void startTimer(){
+        timer = new CountDownTimer(time_left_in_milis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                time_left_in_milis = millisUntilFinished;
+                updateText();
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                timer_running = false;
+                pauseTimer();
+                resetTimer();
+                updateText();
+                userLife = userLife - 1;
+                life.setText("" + userLife);
+                question.setText("Sorry, time is up");
+
+            }
+        }.start();
+
+        timer_running = true;
+    }
+
+    public void updateText()
+    {
+
+        int second = (int)(time_left_in_milis / 1000) % 60;
+        String time_left = String.format(Locale.getDefault(), "%02d", second);
+        time.setText(time_left);
+
+    }
+
+    public void pauseTimer()
+    {
+        timer.cancel();
+        timer_running = false;
+
+
+    }
+
+    public void resetTimer()
+    {
+
+        time_left_in_milis = START_TIMER_IN_MILIS;
+        updateText();
+
 
     }
 }
